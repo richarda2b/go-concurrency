85fcca39c60bceca9804dd2fd3808e354fe9b826
Richard Alvarez <ralvarez@assemblypayments.com>
2020-05-27 20:11:33 +1000

    Adds solution for excercise 1 & 2

--- exc1.go
+++ exc1.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func run() {
 	// Modify the code so you execute task1 and task2
 	// so when you run go test -v you should see both messages
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		task1()
+		wg.Done()
+	}()
+
+	go func() {
+		task2()
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
 
 func task1() {
--- exc2.go
+++ exc2.go
@@ -7,7 +7,18 @@ import (
 func run2() int {
 	// Modify the code so it returns the result of A divided  by B
 	// r: 5
-	return 0
+	c := make(chan int)
+
+	go func() {
+		c <- getA()
+	}()
+
+	go func() {
+		c <- getB()
+	}()
+
+	a, b := <-c, <-c
+	return a / b
 }
 
 func getA() int {
